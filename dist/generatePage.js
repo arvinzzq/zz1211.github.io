cec5971c1192caaaabc795841989c9d89825c08c
zz1211 <zqzhong1211@163.com>
2018-08-02 20:30:44 +0800

    refactor: modify order of issues

--- dist/generatePage.js
+++ dist/generatePage.js
@@ -15,7 +15,10 @@ var _render2 = _interopRequireDefault(_render);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var viewRender = new _render2.default();
-var compMap = { 'top': 1 };
+var compMap = {
+  stick: 10,
+  top: 1
+};
 function calCompWeight(a) {
   var weight = 0;
   a.labels.forEach(function (item) {
@@ -31,5 +34,6 @@ function comp(a, b) {
 console.info('render template into page ->');
 var issueList = JSON.parse(_fs2.default.readFileSync(_path2.default.resolve(process.cwd(), './issues.json')));
 issueList.sort(comp);
+console.log('issueList ----> ', issueList);
 var view = viewRender.render('index', { issueList: issueList });
 _fs2.default.writeFileSync(_path2.default.resolve(process.cwd(), './index.html'), view, 'utf8');
